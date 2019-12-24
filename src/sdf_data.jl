78265aff79b7d0d0ce98f5fe78078cf6ea81980a
Marius Micluta-Campeanu <marius.micluta-campeanu@gitlab.com>
2019-12-24 23:35:18 +0200

    Eliminate reinterpret calls

--- src/sdf_data.jl
+++ src/sdf_data.jl
@@ -2,39 +2,43 @@ function Base.read(f, block::ConstantBlockHeader)
     read(f, block.d_type)
 end
 
-function Base.read(f, block::PlainVariableBlockHeader)
+function Base.read!(f, block::PlainVariableBlockHeader)
     dims = prod(block.npts)
-    seek(f, block.base_header.data_location)
-    raw_data = read(f, dims * sizeof(block.base_header.d_type))
+    raw_data = Array{block.base_header.d_type, 1}(undef, dims)
 
-    reshape(Array(reinterpret(block.base_header.d_type, raw_data)), block.npts)
+    seek(f, block.base_header.data_location)
+    read!(f, raw_data)
+
+    reshape(raw_data, block.npts)
 end
 
-function Base.read(f, block::PointVariableBlockHeader)
+function Base.read!(f, block::PointVariableBlockHeader)
+    raw_data = Array{block.base_header.d_type, 1}(undef, block.npart)
     seek(f, block.base_header.data_location)
-    raw_data = read(f, block.npart * sizeof(block.base_header.d_type))
 
-    Array(reinterpret(block.base_header.d_type, raw_data))
+    read!(f, raw_data)
 end
 
-function Base.read(f, block::PlainMeshBlockHeader)
+function Base.read!(f, block::PlainMeshBlockHeader)
     d_type = block.base_header.d_type
     offset = block.base_header.data_location
 
     ntuple(block.base_header.n_dims) do i
+        raw_data = Array{d_type, 1}(undef, block.npts[i])
         seek(f, offset)
         offset += sizeof(d_type) * block.npts[i]
-        Array(reinterpret(d_type, read(f, block.npts[i]*sizeof(d_type))))
+        read!(f, raw_data)
     end
 end
 
-function Base.read(f, block::PointMeshBlockHeader)
+function Base.read!(f, block::PointMeshBlockHeader)
     d_type = block.base_header.d_type
     offset = block.base_header.data_location
 
     ntuple(block.base_header.n_dims) do _
+        raw_data = Array{d_type, 1}(undef, block.npart)
         seek(f, offset)
         offset += sizeof(d_type) * block.npart
-        Array(reinterpret(d_type, read(f, block.npart*sizeof(d_type))))
+        read!(f, raw_data)
     end
 end
