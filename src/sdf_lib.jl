8602c85b00f48032b5adcc9b838c414b5d5feef7
Marius Micluta-Campeanu <marius.micluta-campeanu@gitlab.com>
2019-12-24 18:34:25 +0200

    Update reader, major refactoring

--- src/sdf_lib.jl
+++ src/sdf_lib.jl
@@ -5,37 +5,37 @@ const SDF_VERSION   = 1
 const SDF_REVISION  = 4
 const SDF_MAGIC     = "SDF1"
 
-const BLOCKTYPE_SCRUBBED                = -1
-const BLOCKTYPE_NULL                    =  0
-const BLOCKTYPE_PLAIN_MESH              =  1
-const BLOCKTYPE_POINT_MESH              =  2
-const BLOCKTYPE_PLAIN_VARIABLE          =  3
-const BLOCKTYPE_POINT_VARIABLE          =  4
-const BLOCKTYPE_CONSTANT                =  5
-const BLOCKTYPE_ARRAY                   =  6
-const BLOCKTYPE_RUN_INFO                =  7
-const BLOCKTYPE_SOURCE                  =  8
-const BLOCKTYPE_STITCHED_TENSOR         =  9
-const BLOCKTYPE_STITCHED_MATERIAL       = 10
-const BLOCKTYPE_STITCHED_MATVAR         = 11
-const BLOCKTYPE_STITCHED_SPECIES        = 12
-const BLOCKTYPE_SPECIES                 = 13
-const BLOCKTYPE_PLAIN_DERIVED           = 14
-const BLOCKTYPE_POINT_DERIVED           = 15
-const BLOCKTYPE_CONTIGUOUS_TENSOR       = 16
-const BLOCKTYPE_CONTIGUOUS_MATERIAL     = 17
-const BLOCKTYPE_CONTIGUOUS_MATVAR       = 18
-const BLOCKTYPE_CONTIGUOUS_SPECIES      = 19
-const BLOCKTYPE_CPU_SPLIT               = 20
-const BLOCKTYPE_STITCHED_OBSTACLE_GROUP = 21
-const BLOCKTYPE_UNSTRUCTURED_MESH       = 22
-const BLOCKTYPE_STITCHED                = 23
-const BLOCKTYPE_CONTIGUOUS              = 24
-const BLOCKTYPE_LAGRANGIAN_MESH         = 25
-const BLOCKTYPE_STATION                 = 26
-const BLOCKTYPE_STATION_DERIVED         = 27
-const BLOCKTYPE_DATABLOCK               = 28
-const BLOCKTYPE_NAMEVALUE               = 29
+const BLOCKTYPE_SCRUBBED                = Int32(-1)
+const BLOCKTYPE_NULL                    =  Int32(0)
+const BLOCKTYPE_PLAIN_MESH              =  Int32(1)
+const BLOCKTYPE_POINT_MESH              =  Int32(2)
+const BLOCKTYPE_PLAIN_VARIABLE          =  Int32(3)
+const BLOCKTYPE_POINT_VARIABLE          =  Int32(4)
+const BLOCKTYPE_CONSTANT                =  Int32(5)
+const BLOCKTYPE_ARRAY                   =  Int32(6)
+const BLOCKTYPE_RUN_INFO                =  Int32(7)
+const BLOCKTYPE_SOURCE                  =  Int32(8)
+const BLOCKTYPE_STITCHED_TENSOR         =  Int32(9)
+const BLOCKTYPE_STITCHED_MATERIAL       = Int32(10)
+const BLOCKTYPE_STITCHED_MATVAR         = Int32(11)
+const BLOCKTYPE_STITCHED_SPECIES        = Int32(12)
+const BLOCKTYPE_SPECIES                 = Int32(13)
+const BLOCKTYPE_PLAIN_DERIVED           = Int32(14)
+const BLOCKTYPE_POINT_DERIVED           = Int32(15)
+const BLOCKTYPE_CONTIGUOUS_TENSOR       = Int32(16)
+const BLOCKTYPE_CONTIGUOUS_MATERIAL     = Int32(17)
+const BLOCKTYPE_CONTIGUOUS_MATVAR       = Int32(18)
+const BLOCKTYPE_CONTIGUOUS_SPECIES      = Int32(19)
+const BLOCKTYPE_CPU_SPLIT               = Int32(20)
+const BLOCKTYPE_STITCHED_OBSTACLE_GROUP = Int32(21)
+const BLOCKTYPE_UNSTRUCTURED_MESH       = Int32(22)
+const BLOCKTYPE_STITCHED                = Int32(23)
+const BLOCKTYPE_CONTIGUOUS              = Int32(24)
+const BLOCKTYPE_LAGRANGIAN_MESH         = Int32(25)
+const BLOCKTYPE_STATION                 = Int32(26)
+const BLOCKTYPE_STATION_DERIVED         = Int32(27)
+const BLOCKTYPE_DATABLOCK               = Int32(28)
+const BLOCKTYPE_NAMEVALUE               = Int32(29)
 
 const DATATYPE_NULL      = 0
 const DATATYPE_INTEGER4  = 1
@@ -68,24 +68,71 @@ struct Header
 end
 
 struct BlockHeader
-  next_block_location   
-  data_location         
-  id                  
-  data_length         
-  block_type          
-  data_type           
-  n_dims              
-  name
+  next_block_location::Int64
+  data_location::Int64
+  id::String
+  data_length::Int64
+  block_type::Int32
+  d_type::DataType
+  n_dims::Int32
+  name::String
 end
 
-abstract type AbstractDataHeader end
+abstract type AbstractBlockHeader end
 
-struct ConstantDataHeader <: AbstractDataHeader
-  header::BlockHeader
+struct ConstantBlockHeader <: AbstractBlockHeader
+  base_header::BlockHeader
 end
 
-struct PlainVariableDataHeader <: AbstractDataHeader
-  header::BlockHeader
+struct CPUSplitBlockHeader <: AbstractBlockHeader
+  base_header::BlockHeader
+end
+
+struct PlainVariableBlockHeader{N} <: AbstractBlockHeader
+  base_header::BlockHeader
+
+  mult::Float64
+  units::String
+  mesh_id::String
+  npts::Array{Int32, N}
+  stagger::Int32
+end
+
+struct PointVariableBlockHeader <: AbstractBlockHeader
+  base_header::BlockHeader
+
+  mult::Float64
+  units::String
+  mesh_id::String
+  npart::Int64
+end
+
+struct PlainMeshBlockHeader{N} <: AbstractBlockHeader
+  base_header::BlockHeader
+  
+  mults::Array{Float64, N}
+  labels::Array{String, N}
+  units::Array{String, N}
+  geometry::Int32
+  minval::Array{Float64, N}
+  maxval::Array{Float64, N}
+  npts::Array{Int32, N}
+end
+
+struct PointMeshBlockHeader{N} <: AbstractBlockHeader
+  base_header::BlockHeader
+
+  mults::Array{Float64, N}
+  labels::Array{String, N}
+  units::Array{String, N}
+  geometry::Int32
+  minval::Array{Float64, N}
+  maxval::Array{Float64, N}
+  npart::Int64
+end
+
+struct CPUInfoBlockHeader <: AbstractBlockHeader
+  base_header::BlockHeader
 end
 
 function header!(f)
@@ -93,7 +140,7 @@ function header!(f)
   endianness            = read(f, Int32)
   version               = read(f, Int32)
   revision              = read(f, Int32)
-  code_name             = rstrip(String(read(f, ID_LENGTH)))
+  code_name             = (String(read(f, ID_LENGTH)))
   first_block_location  = read(f, Int64)
   summary_location      = read(f, Int64)
   summary_size          = read(f, Int32)
@@ -122,39 +169,54 @@ function type_from(data_type)
     Int32
   elseif data_type == DATATYPE_INTEGER8
     Int64
+  else
+    Nothing
   end
 end
 
-function read_constant!(f, d_type)
-  read(f, d_type)
+function read_header!(f, block, ::Val{BLOCKTYPE_CONSTANT})
+  ConstantBlockHeader(block)
+end
+
+function read_data!(f, block, ::Val{BLOCKTYPE_CONSTANT})
+  read(f, block.d_type)
 end
 
 function read_variable_common!(f)
   mult    = read(f, Float64)
-  units   = rstrip(String(read(f, ID_LENGTH)))
-  mesh_id = rstrip(String(read(f, ID_LENGTH)))
+  units   = String(read(f, ID_LENGTH))
+  mesh_id = String(read(f, ID_LENGTH))
+
   (mult, units, mesh_id)
 end
 
-function read_plain_variable!(f, d_type, data_location, n_dims)
+function read_header!(f, block, ::Val{BLOCKTYPE_PLAIN_VARIABLE})
   mult, units, mesh_id = read_variable_common!(f)
 
-  npts = reinterpret(Int32, read(f, n_dims*sizeof(Int32)))
-  dims = prod(npts)
+  npts = reinterpret(Int32, read(f, block.n_dims*sizeof(Int32)))
   stagger = read(f, Int32)
 
-  # println("$mult \t $units \t $mesh_id \t $npts \t $stagger\t$dims\t$d_type\t$data_location")
-  seek(f, data_location)
-  reinterpret(d_type, read(f, dims*sizeof(d_type)))
+  PlainVariableBlockHeader(block, mult, units, mesh_id, Array(npts), stagger)
 end
 
-function read_point_variable!(f, d_type, data_location)
+function read_data!(f, block, ::Val{BLOCKTYPE_PLAIN_VARIABLE})
+  dims = prod(block.npts)
+  seek(f, block.data_location)
+
+  reinterpret(block.d_type, read(f, dims*sizeof(block.d_type)))
+end
+
+function read_header!(f, block, ::Val{BLOCKTYPE_POINT_VARIABLE})
   mult, units, mesh_id = read_variable_common!(f)
   npart   = read(f, Int64)
 
-  # println("$mult \t $units \t $mesh_id \t $npart \t $d_type \t $data_location")
-  seek(f, data_location)
-  reinterpret(d_type, read(f, npart*sizeof(d_type)))
+  PointVariableBlockHeader(block, mult, units, mesh_id, npart)
+end
+
+function read_data!(f, block, ::Val{BLOCKTYPE_POINT_VARIABLE})
+  seek(f, block.data_location)
+
+  reinterpret(block.d_type, read(f, block.npart*sizeof(block.d_type)))
 end
 
 function read_mesh_common!(f, n)
@@ -164,36 +226,54 @@ function read_mesh_common!(f, n)
   mults = reinterpret(Float64, read(f, n*sizeof(Float64)))
 
   for i = 1:n
-    labels[i] = rstrip(String(read(f, ID_LENGTH)))
+    labels[i] = String(read(f, ID_LENGTH))
   end
   for i = 1:n
-    units[i] = rstrip(String(read(f, ID_LENGTH)))
+    units[i] = String(read(f, ID_LENGTH))
   end
   geometry = read(f, Int32)
-  extents = reinterpret(Float64, read(f, 2*n*sizeof(Float64)))
+  minval = reinterpret(Float64, read(f, n*sizeof(Float64)))
+  maxval = reinterpret(Float64, read(f, n*sizeof(Float64)))
 
-  (mults, labels, units, geometry, extents)
+  (mults, labels, units, geometry, minval, maxval)
+end
+
+function read_header!(f, block, ::Val{BLOCKTYPE_PLAIN_MESH})
+  mults, labels, units, geometry, minval, maxval = read_mesh_common!(f, block.n_dims)
+  npts = reinterpret(Int32, read(f, block.n_dims*sizeof(Int32)))
+
+  PlainMeshBlockHeader(
+    block, Array(mults), labels, units, geometry,
+    Array(minval), Array(maxval), Array(npts)
+  )
 end
 
 function read_plain_mesh!(f, d_type, data_location, data_length, n)
-  mults, labels, units, geometry, extents = read_mesh_common!(f, n)
-
-  npts = reinterpret(Int32, read(f, n*sizeof(Int32)))
   n_elems = sum(npts)  # prod?
   type_size = div(data_length, n_elems)
   offset = data_location
   for i = 1:n
     #seek(f, offset)
     #read(f, npts[i] n_elems)
-    print(offset, " ")
+    # print(offset, " ")
     offset = offset + type_size*npts[i]
   end
-  println(" $geometry\t$mults\t$labels\t$units\t$extents\t$npts")
+
   (data_location, d_type, n_elems)
 end
 
+function read_header!(f, block, ::Val{BLOCKTYPE_POINT_MESH})
+  mults, labels, units, geometry, minval, maxval = read_mesh_common!(f, block.n_dims)
+  npart = read(f, Int64)
+
+  PointMeshBlockHeader(
+    block, Array(mults), labels, units, geometry,
+    Array(minval), Array(maxval), npart
+  )
+end
+
 function read_point_mesh!(f, d_type, data_location, data_length, n)
-  mults, labels, units, geometry, extents = read_mesh_common!(f, n)
+  mults, labels, units, geometry, minval, maxval = read_mesh_common!(f, n)
 
   npart = read(f, Int64)
 
@@ -203,70 +283,64 @@ function read_point_mesh!(f, d_type, data_location, data_length, n)
   for i = 1:n
     #seek(f, offset)
     #read(f, npts[i] n_elems)
-    print(offset, " ")
+    # print(offset, " ")
     offset = offset + type_size*npart
   end
-  println(" $geometry\t$mults\t$labels\t$units\t$extents\t$npart")
+
   (data_location, d_type, n_elems)
 end
 
-function read_run_info!(f)
+function read_header!(f, block, ::Val{BLOCKTYPE_RUN_INFO})
   code_version    = read(f, Int32)
   code_revision   = read(f, Int32)
-  commit_id       = rstrip(String(read(f, ID_LENGTH)))
-  sha1sum         = rstrip(String(read(f, ID_LENGTH)))
-  compile_machine = rstrip(String(read(f, ID_LENGTH)))
-  compile_flags   = rstrip(String(read(f, ID_LENGTH)))
+  commit_id       = String(read(f, ID_LENGTH))
+  sha1sum         = String(read(f, ID_LENGTH))
+  compile_machine = String(read(f, ID_LENGTH))
+  compile_flags   = String(read(f, ID_LENGTH))
   defines         = read(f, Int64)
   compile_date    = read(f, Int32)
   run_date        = read(f, Int32)
   io_date         = read(f, Int32)
-  println("$code_version, $code_revision, $commit_id, $sha1sum, $compile_machine, $compile_flags, $defines, $compile_date, $run_date, $io_date")
-  nothing
+
+  CPUInfoBlockHeader(block)
 end
 
-function n(filename)
+function read_header!(f, block, ::Val{BLOCKTYPE_CPU_SPLIT})
+  CPUSplitBlockHeader(block)
+end
+
+function fetch_blocks(filename)
   open(filename, "r") do f
     r = header!(f)
+    blocks = Array{AbstractBlockHeader}(undef, r.nblocks)
 
     block_start = r.first_block_location
-    println("nxt_bl\tdat_loc\tdat_len\tblk_typ\tdt_typ\tndims\tvar\t\t\t\t\t\tid")
     for i = 1:r.nblocks
       seek(f, block_start)
-      next_block_location = read(f, UInt64)
-      data_location       = read(f, UInt64)
-      id                  = rstrip(String(read(f, ID_LENGTH)))
-      data_length         = read(f, UInt64)
-      block_type          = read(f, UInt32)
-      data_type           = read(f, UInt32)
-      n_dims              = convert(Int64, read(f, UInt32))
-      name                = rstrip(String(read(f, r.string_length)))
+      next_block_location = read(f, Int64)
+      data_location       = read(f, Int64)
+      id                  = String(read(f, ID_LENGTH))
+      data_length         = read(f, Int64)
+      block_type          = read(f, Int32)
+      data_type           = read(f, Int32)
+      n_dims              = read(f, Int32)
+      name                = String(read(f, r.string_length))
 
       seek(f, block_start + r.block_header_length)
       d_type = type_from(data_type)
 
-      var = if block_type == BLOCKTYPE_CONSTANT
-              read_constant!(f, d_type)
-            elseif block_type == BLOCKTYPE_PLAIN_VARIABLE
-              read_plain_variable!(f, d_type, data_location, n_dims)
-            elseif block_type == BLOCKTYPE_POINT_VARIABLE
-              read_point_variable!(f, d_type, data_location)
-            elseif block_type == BLOCKTYPE_PLAIN_MESH
-              read_plain_mesh!(f, d_type, data_location, data_length, n_dims)
-            elseif block_type == BLOCKTYPE_POINT_MESH
-              read_point_mesh!(f, d_type, data_location, data_length, n_dims)
-            elseif block_type == BLOCKTYPE_RUN_INFO
-              read_run_info!(f)
-            #elseif block_type == BLOCKTYPE_CPU_SPLIT
-            #  read_cpu_split!(f, d_type, data_location)
-            end
-      println("$next_block_location \t $data_location\t$data_length \t $block_type \t $data_type \t $n_dims\t$var\t$id")
+      block = BlockHeader(
+        next_block_location, data_location, id, data_length,
+        block_type, d_type, n_dims, name
+      )
+      blocks[i] = read_header!(f, block, Val(block_type))
       block_start = next_block_location
     end
-    r
+
+    blocks
   end
 end
 
-function m()
-  n(ARGS[1])
+function main()
+  fetch_blocks(ARGS[1])
 end
