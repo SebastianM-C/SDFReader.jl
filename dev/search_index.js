dd18628d6fa1b6c81d8377b73ba2cff76bbb2fb2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-11-12 13:45:09 +0000

    build based on 4d1af66

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SDFReader.jl-1","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"section"},{"location":"#","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"page"},{"location":"#","page":"SDFReader.jl","title":"SDFReader.jl","text":"Modules = [SDFReader]","category":"page"}]
+}
