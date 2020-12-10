41bf1522a6a13503331f1dda61f942460566a3dd
Documenter.jl <documenter@juliadocs.github.io>
2020-12-10 21:13:20 +0000

    build based on a869089

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SDFReader.jl-1","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"section"},{"location":"#","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"page"},{"location":"#","page":"SDFReader.jl","title":"SDFReader.jl","text":"Modules = [SDFReader]","category":"page"}]
+[{"location":"#SDFReader.jl","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"section"},{"location":"","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"page"},{"location":"","page":"SDFReader.jl","title":"SDFReader.jl","text":"Modules = [SDFReader]","category":"page"}]
 }
