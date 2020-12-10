07b825ef56cfe39fac9d21e4ac73e52b3d55e46a
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-12-10 22:36:31 +0200

    Update tests

--- test/api.jl
+++ test/api.jl
@@ -2,19 +2,26 @@ using Unitful
 
 @testset "Public API tests" begin
     dir = pwd()
-    file = "0002.sdf"
-    blocks = file_summary(file)
-    @test getindex(blocks, "ex") isa SDFReader.AbstractBlockHeader
+    sim = read_simulation(dir)
+    @test sim isa EPOCHSimulation
+    file = sim[1]
+    @test file isa SDFFile
+    Ex = sim[1][:ex]
+    @test Ex isa ScalarField{3}
+    @test all(unit.(Ex.data) .== u"V/m")
+    @test all(unit.(Ex.grid[1]) .== u"m")
 
-    k = ["grid/electron",
-        "py/electron",
-        "pz/electron",
-        "ex"]
-    (x,y,z), py, pz, ex = readkeys(file, blocks, k)
+    vars = (:grid, Symbol("py/electron"), Symbol("pz/electron"))
+    @task all(vars .∈ keys(file))
+    (x,y,z), py, pz = read(file, vars...)
     @test all(unit.(x) .== u"m")
     @test all(unit.(py) .== u"kg*m/s")
-    @test all(unit.(ex) .== u"V/m")
 
     t = get_time(file)
     @test (t |> u"fs") ≈ 10u"fs" atol = 0.1u"fs"
+
+    nx = get_parameter(file, :nx)
+    @test nx = 10
+    λ = get_parameter(file, :laser, :lambda)
+    @test (λ |> u"nm") ≈ 800u"nm"
 end
--- test/sdf.jl
+++ test/sdf.jl
@@ -1,22 +1,22 @@
 @testset "SDF" begin
     include("sdf_header.jl")
     fn = "0002.sdf"
-    blocks = SDF.file_summary(fn)
+    blocks = file_summary(fn)
 
     @testset "Units" begin
-        for (key, block) in blocks
+        for (key, block) in pairs(blocks)
             if @compat hasfield(typeof(block), :units)
-                @test block.units == units[key]
+                @test block.units == units[string(key)]
             end
         end
     end
 
-    unsupported = ["cpu/proton", "cpu/electron", "run_info", "cpu_rank"]
+    unsupported = Symbol.(["cpu/proton", "cpu/electron", "run_info", "cpu_rank"])
     @testset "Data" begin
         open(fn, "r") do f
-            @testset "$key" for (key, block) in blocks
+            @testset "$key" for (key, block) in pairs(blocks)
                 if key ∉ unsupported
-                    @test read!(f, block) == data[key]
+                    @test read!(f, block) == data[string(key)]
                 end
             end
         end
