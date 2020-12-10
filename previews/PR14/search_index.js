21264fc8cd1e13f5a42d48b8d8e0fde1b34d1116
Documenter.jl <documenter@juliadocs.github.io>
2020-12-10 21:33:04 +0000

    build based on 8d97af7

---
+++ previews/PR14/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SDFReader.jl","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"section"},{"location":"","page":"SDFReader.jl","title":"SDFReader.jl","text":"","category":"page"},{"location":"","page":"SDFReader.jl","title":"SDFReader.jl","text":"Modules = [SDFReader]","category":"page"}]
+}
