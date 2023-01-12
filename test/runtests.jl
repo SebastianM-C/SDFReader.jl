ce554c7a9bd80cd6e896722c848e83e5cee8e2e4
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2023-01-12 11:32:06 +0200

    actually run the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,5 @@ using SafeTestsets
     @safetestset "SDF header" begin include("sdf_header.jl") end
     @safetestset "SDF data" begin include("sdf.jl") end
     @safetestset "DiskArrays integration" begin include("chunks.jl") end
+    @safetestset "mmap" begin include("mmap.jl") end
 end
